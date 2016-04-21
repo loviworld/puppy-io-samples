d789d18ff5d7046d081e08ea6beadbf5e2056a4a
Tharanga <tharanganilupul@gmail.com>
2016-04-21 20:18:18 +0530

    modified IndexController

--- src/main/java/com/lovi/um/controller/IndexController.java
+++ src/main/java/com/lovi/um/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.lovi.um.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import com.lovi.puppy.annotation.Controller;
+import com.lovi.puppy.annotation.ModelAttribute;
 import com.lovi.puppy.annotation.RequestMapping;
 import com.lovi.puppy.annotation.RequestParm;
 import com.lovi.puppy.annotation.enums.HttpMethod;
@@ -21,7 +22,7 @@ public class IndexController {
 	private ServiceCaller serviceCaller;
 	
 	@RequestMapping
-	public void index(Session session, ViewAttribute viewAttribute, HttpResponseResult responseResult){
+	public void loadIndexView(Session session, ViewAttribute viewAttribute, HttpResponseResult responseResult){
 		User loggedUser = session.get("user", User.class);
 		if(loggedUser != null){
 			viewAttribute.put("loggedUser", loggedUser);
@@ -51,6 +52,33 @@ public class IndexController {
 		
 		failResult.setHandler(fail->{
 			responseResult.complete("/");
+		});
+		
+	}
+	
+	@RequestMapping("/sign-up")
+	public void loadSignUpView(HttpResponseResult responseResult){
+		responseResult.complete("sign-up");
+	}
+	
+	@RequestMapping(value="/sign-up",method=HttpMethod.POST)
+	public void signUp(	@ModelAttribute User user,
+						Session session,
+						ViewAttribute viewAttribute,
+						HttpResponseResult responseResult) throws ServiceCallerException{
+		
+		Result<User> result = Result.create();
+		FailResult failResult = FailResult.create();
+		
+		serviceCaller.call("UserService.insert", result, user);
+		
+		result.process(newUser->{
+			responseResult.complete("/");
+		}, failResult);
+		
+		failResult.setHandler(fail->{
+			viewAttribute.put("message", "fail - duplicate user");
+			responseResult.complete("/sign-up");
 		});
 		
 	}
